9bfc74779eb592153ff30a6d8d3ac56ceb3c9ccc
devplus2024 <anpham08112009@gmail.com>
2025-02-07 17:13:18 +0700

    df

--- src/app/docs-x/[[...slug]]/page.tsx
+++ src/app/docs-x/[[...slug]]/page.tsx
@@ -2,22 +2,21 @@ import { source } from '@/lib/source';
 import {
   DocsPage,
   DocsBody,
-  DocsTitle,
   DocsDescription,
+  DocsTitle,
 } from 'fumadocs-ui/page';
 import { notFound } from 'next/navigation';
 import defaultMdxComponents from 'fumadocs-ui/mdx';
-import { metadataImage } from '@/lib/metadata-image';
- 
+
 export default async function Page(props: {
   params: Promise<{ slug?: string[] }>;
 }) {
   const params = await props.params;
   const page = source.getPage(params.slug);
   if (!page) notFound();
- 
+
   const MDX = page.data.body;
- 
+
   return (
     <DocsPage toc={page.data.toc} full={page.data.full}>
       <DocsTitle>{page.data.title}</DocsTitle>
@@ -28,20 +27,20 @@ export default async function Page(props: {
     </DocsPage>
   );
 }
- 
+
 export async function generateStaticParams() {
   return source.generateParams();
 }
- 
+
 export async function generateMetadata(props: {
   params: Promise<{ slug?: string[] }>;
 }) {
   const params = await props.params;
   const page = source.getPage(params.slug);
   if (!page) notFound();
- 
-  return metadataImage.withImage(page.slugs, {
+
+  return {
     title: page.data.title,
     description: page.data.description,
-  });
+  };
 }
