c3f35af5f00d369c0731ea8a79617c9f11e7ccc4
devplus2024 <anpham08112009@gmail.com>
2025-02-12 11:16:53 +0700

    cv

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -43,10 +43,10 @@ export default function RootLayout({
         <body className="relative overflow-x-hidden antialiased">
         <AnimatePresence mode="wait">
           <motion.div key={pathname}>
-          <motion.div className="slide-in" initial={{scaleY:0}} animate={{scaleY:0}} exit={{scaleY:1}} transition={{duration:1,ease:[0.22,1,0.36,1]}}>
+          <motion.div className="slide-in" initial={{scaleY:0}} animate={{scaleY:0}} exit={{scaleY:1}} transition={{duration:0.5,ease:[0.22,1,0.36,1]}}>
 
 </motion.div>
-<motion.div className="slide-out" initial={{scaleY:1}} animate={{scaleY:0}} exit={{scaleY:0}} transition={{duration:1,ease:[0.22,1,0.36,1]}}>
+<motion.div className="slide-out" initial={{scaleY:1}} animate={{scaleY:0}} exit={{scaleY:0}} transition={{duration:0.5,ease:[0.22,1,0.36,1]}}>
 
 </motion.div>
      <CookieAlert />
