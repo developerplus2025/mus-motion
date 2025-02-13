c8ed35e5d449c4dcb137ff3c11f85d9bed6788fa
devplus2024 <anpham08112009@gmail.com>
2025-02-13 17:14:14 +0700

    cv

--- src/app/pricing/individual.tsx
+++ src/app/pricing/individual.tsx
@@ -1125,7 +1125,23 @@ export default function Individual() {
                 {tier.features.map((feature) => (
                   <li key={feature.name} className="flex px-2 pb-2 border-b items-center gap-2">
                     <div className="rounded-md  p-[4px]">
-                      {feature.icon}
+                      {/* {feature.icon} */}
+                      <svg
+  data-testid="geist-icon"
+  height={16}
+  strokeLinejoin="round"
+  viewBox="0 0 16 16"
+  width={16}
+  style={{ color: "currentcolor" }}
+>
+  <path
+    fillRule="evenodd"
+    clipRule="evenodd"
+    d="M16 8C16 12.4183 12.4183 16 8 16C3.58172 16 0 12.4183 0 8C0 3.58172 3.58172 0 8 0C12.4183 0 16 3.58172 16 8ZM11.5303 6.53033L12.0607 6L11 4.93934L10.4697 5.46967L6.5 9.43934L5.53033 8.46967L5 7.93934L3.93934 9L4.46967 9.53033L5.96967 11.0303C6.26256 11.3232 6.73744 11.3232 7.03033 11.0303L11.5303 6.53033Z"
+    fill="currentColor"
+  />
+</svg>
+
                       {/* <CheckIcon className="h-[15px] w-[15px] flex-shrink-0 text-[#ffffff]" /> */}
                     </div>
                     <p className="text-sm text-[white]">{feature.name}</p>
