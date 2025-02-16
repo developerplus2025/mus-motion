2c565f355385c2d4c7d836005a130da1ba4b1197
devplus2024 <anpham08112009@gmail.com>
2025-02-16 11:52:08 +0700

    vb

--- src/app/home/components/loop-animation.tsx
+++ src/app/home/components/loop-animation.tsx
@@ -3,5 +3,5 @@ import UseAnimations from "react-useanimations";
 import infinity from 'react-useanimations/lib/infinity';
 
 export const LoopAnimation = () => (
-  <UseAnimations strokeColor="white" fillColor="#ffffff" animation={infinity} size={24} />
+  <UseAnimations strokeColor="white" fillColor="#ffffff" animation={infinity} size={34} />
 );
