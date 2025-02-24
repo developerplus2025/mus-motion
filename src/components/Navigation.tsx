d8bb226c2da705023d9ace28bd99e24f5bdcf2e4
devplus2024 <anpham08112009@gmail.com>
2025-02-24 15:13:06 +0700

    bn

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -219,7 +219,8 @@ export const Navigation = (): JSX.Element => {
         isSignIn ||
         isGuides ||
         isDocs ||
-        isHelp || pathname.startsWith("/docs")
+        isHelp ||
+        pathname.startsWith("/docs")
           ? "webfilm-class"
           : ""
       } sticky top-0 z-[20]`}
@@ -352,22 +353,22 @@ export const Navigation = (): JSX.Element => {
                   </Link>
                 </NavigationMenuItem>
                 <NavigationMenuItem>
-                  <Link href="/community" legacyBehavior passHref>
+                  <Link href="/blog" legacyBehavior passHref>
                     <NavigationMenuLink
                       className={`relative flex h-7 items-center rounded-full px-3 text-sm duration-300 ease-out ${
-                        pathname === "/community"
+                        pathname === "/blog"
                           ? "dark:text-white"
                           : "dark:text-[#9b9b9b]"
                       } cursor-pointer select-none transition-colors dark:hover:text-white`}
                     >
                       <TextHoverEnter
                         className={`${
-                          pathname === "/community"
+                          pathname === "/blog"
                             ? "dark:text-white"
                             : "dark:text-[#9b9b9b]"
                         }`}
                       >
-                        Community
+                        Blog
                       </TextHoverEnter>
                     </NavigationMenuLink>
                   </Link>
@@ -603,27 +604,25 @@ export const Navigation = (): JSX.Element => {
                   animate={{ opacity: 1 }} // Trạng thái sau khi hoàn thành: rõ và về vị trí ban đầu
                   transition={{ duration: 0.5, ease: "easeOut" }}
                 >
-                  <Button  className="h-fit [&_svg]:size-2 gap-1 px-3 py-1">
-                  Sign Up
-                  <svg
-  data-testid="geist-icon"
-  height={12}
-  strokeLinejoin="round"
-  viewBox="0 0 16 16"
-  width={12}
-  style={{ color: "currentcolor" }}
->
-  <path
-    fillRule="evenodd"
-    clipRule="evenodd"
-    d="M5.50001 1.93933L6.03034 2.46966L10.8536 7.29288C11.2441 7.68341 11.2441 8.31657 10.8536 8.7071L6.03034 13.5303L5.50001 14.0607L4.43935 13L4.96968 12.4697L9.43935 7.99999L4.96968 3.53032L4.43935 2.99999L5.50001 1.93933Z"
-    fill="currentColor"
-  />
-</svg>
-
+                  <Button className="h-fit gap-1 px-3 py-1 [&_svg]:size-2">
+                    Sign Up
+                    <svg
+                      data-testid="geist-icon"
+                      height={12}
+                      strokeLinejoin="round"
+                      viewBox="0 0 16 16"
+                      width={12}
+                      style={{ color: "currentcolor" }}
+                    >
+                      <path
+                        fillRule="evenodd"
+                        clipRule="evenodd"
+                        d="M5.50001 1.93933L6.03034 2.46966L10.8536 7.29288C11.2441 7.68341 11.2441 8.31657 10.8536 8.7071L6.03034 13.5303L5.50001 14.0607L4.43935 13L4.96968 12.4697L9.43935 7.99999L4.96968 3.53032L4.43935 2.99999L5.50001 1.93933Z"
+                        fill="currentColor"
+                      />
+                    </svg>
                     {/* <Sparkles />
                     Ask Ai */}
-                   
                   </Button>
                 </motion.div>
               </Link>
