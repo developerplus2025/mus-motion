1e160a9c33eae9fd2fa5c6942dd413ce14a21dca
devplus2024 <anpham08112009@gmail.com>
2025-03-03 20:10:32 +0700

    vb

--- src/app/home/components/rich-text-editor.tsx
+++ src/app/home/components/rich-text-editor.tsx
@@ -292,7 +292,7 @@ export default function RichTextEditor() {
           </Button>
         </div>
 
-        <CardContent className="rounded-lg p-4 dark:bg-black">
+        <CardContent className="rounded-lg p-4 dark:bg-[#131313]">
           <div
             className="min-h-[300px] rounded-md border p-3 focus-within:ring-1 focus-within:ring-ring dark:border dark:bg-black dark:text-gray-200"
             onClick={() => {
