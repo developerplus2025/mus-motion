644f8a2437795599862e08a3cbe8a5bc35e3536c
devplus2024 <anpham08112009@gmail.com>
2025-03-05 21:29:36 +0700

    fg

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import type { Metadata } from "next";
 // import { usePathname } from "next/navigation";
 // import { AnimatePresence, motion } from "framer-motion";
 import { Inter } from "next/font/google";
+import { unstable_ViewTransition as ViewTransition } from "react";
 import "./globals.css";
 // import 'fumadsocs-ui/dist/style.css';
 import { GeistSans } from "geist/font/sans";
@@ -18,7 +19,7 @@ import Providers from "@/components/ProgressBarProvider";
 import Footer from "@/components/footer";
 import { ToasterSonner } from "../components/ui/sonner";
 import CookieAlert from "@/components/cookie-alert";
-import { RootProvider } from 'fumadocs-ui/provider';
+import { RootProvider } from "fumadocs-ui/provider";
 import FrameVideo from "./home/components/frame-video";
 const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
@@ -41,7 +42,7 @@ export default function RootLayout({
       >
         <head />
         <body className="relative overflow-x-hidden antialiased">
-        {/* <AnimatePresence mode="wait">
+          {/* <AnimatePresence mode="wait">
           <motion.div key={pathname}> */}
           {/* <motion.div className="slide-in" initial={{scaleY:0}} animate={{scaleY:0}} exit={{scaleY:1}} transition={{duration:0.5,ease:[0.22,1,0.36,1]}}>
 
@@ -49,28 +50,30 @@ export default function RootLayout({
 <motion.div className="slide-out" initial={{scaleY:1}} animate={{scaleY:0}} exit={{scaleY:0}} transition={{duration:0.5,ease:[0.22,1,0.36,1]}}>
 
 </motion.div> */}
-     <CookieAlert />
-     <ThemeProvider enableSystem attribute="class" defaultTheme="dark">
-       <Navigation />
-       <DocsNavigation />
-       <Toaster />
-       <ToasterSonner
-         hotkey={["alt + C"]}
-         toastOptions={{
-           unstyled: false,
-           classNames: {
-             cancelButton: "bg-orange-400",
-             closeButton:
-               "dark:bg-black left-[325px] top-1/2 -translate-y-1/2 border border-[#404040] transition-[background] transition-colors  ease-out duration-500 bg-white hover:bg-muted dark:hover:bg-white dark:hover:text-black",
-           },
-         }}
-         closeButton
-         className="top-[60px]"
-         position="top-center"
-       />
-       <RootProvider>{children}</RootProvider>
-       <Footer />
-     </ThemeProvider>
+          <CookieAlert />
+          <ThemeProvider enableSystem attribute="class" defaultTheme="dark">
+            <Navigation />
+            <DocsNavigation />
+            <Toaster />
+            <ToasterSonner
+              hotkey={["alt + C"]}
+              toastOptions={{
+                unstyled: false,
+                classNames: {
+                  cancelButton: "bg-orange-400",
+                  closeButton:
+                    "dark:bg-black left-[325px] top-1/2 -translate-y-1/2 border border-[#404040] transition-[background] transition-colors  ease-out duration-500 bg-white hover:bg-muted dark:hover:bg-white dark:hover:text-black",
+                },
+              }}
+              closeButton
+              className="top-[60px]"
+              position="top-center"
+            />
+            <RootProvider>
+              <ViewTransition>{children}</ViewTransition>
+            </RootProvider>
+            <Footer />
+          </ThemeProvider>
           {/* </motion.div>
      
           </AnimatePresence> */}
